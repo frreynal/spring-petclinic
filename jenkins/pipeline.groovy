fd2ade99b801b7623f16d3bbf6e4d8197f5ac182
frreynal <francois.reynal@outlook.fr>
2021-05-16 15:32:36 +0200

    Commit 4

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -36,7 +36,11 @@ pipeline {
         // failed, record the test results and archive the jar file.
         always {
             junit '**/target/surefire-reports/TEST-*.xml'
-            emailext attachLog: true, body: ' Please go to ${BUILD_URL} and verify the build', compressLog: true, recipientProviders: [requestor(), upstreamDevelopers()], subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input'
+            emailext attachLog: true,
+                    body: ' Please go to ${BUILD_URL} and verify the build',
+                    compressLog: true,
+                    recipientProviders: [requestor(), upstreamDevelopers()],
+                    subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input'
         }
         success {
             archiveArtifacts 'target/*.jar'
